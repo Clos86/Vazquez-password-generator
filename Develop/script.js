df30a96fc6667cea15bc8e292c3202f206cdcf3c
Carlos Vazquez <c.vazquez1986@gmail.com>
2022-01-10 22:07:00 -0500

    adding if and else part 1

--- Develop/script.js
+++ Develop/script.js
@@ -13,5 +13,64 @@ function writePassword() {
 
 }
 
+function generatePassword() {
+  var returnPassword = "";
+  // THEN I am presented with a series of prompts for password criteria
+ 
+  // Prompt the user for password length
+  var passwordLength = getPasswordLengthFromUser();
+
+  // check whatever the user gave us is between 8-128 characters
+  if (passwordLengthIsValid(passwordLength)) {
+    // THEN I confirm whether or not to include lowercase, uppercase, numeric, and/or special characters
+    var includeLowercase = confirm("Click OK if you want to include lowercase in your password.");
+    var includeUppercase = confirm("Click OK if you want to include uppercase in your password.");
+    var includeNumeric = confirm("Click OK if you want to include numbers in your password.");
+    var includeSpecialChar = confirm("Click OK if you want to include special characters in your password.");    
+  } else {
+    alert("Invalid number, please try again.");
+  }
+
+  // THEN my input should be validated and at least one character type should be selected
+  if (includeLowercase || includeUppercase || includeNumeric || includeSpecialChar) {
+    // If we got inside here then at least one character type was selected.
+  } else {
+    alert("You need to at least pick on character type, please try again.");
+  }
+
+  // WHEN all prompts are answered
+  // THEN a password is generated that matches the selected criteria
+
+  return passwordLength;
+
+}
+
+function getPasswordLengthFromUser() {
+  // Prompt user
+  var userResponse = prompt("Select a password length between 8 and 128.");
+  return userResponse;
+}
+
+function passwordLengthIsValid(passwordLength) {
+  console.log ("Password length is " + passwordLength + ".");
+  if (passwordLength < 8) {
+    return false;
+  }
+
+  if (passwordLength > 128) {
+    return false;
+  }
+
+  return true;
+}
+
+function generatePasswordFromUserInput(passwordLength, includeLowercase, includeUppercase, includeNumeric, includeSpecialChar) {
+  var returnPassword = "";
+while ()
+  return returnPassword;
+}
+
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+
